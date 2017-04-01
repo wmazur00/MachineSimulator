1b91917e82eae83927f091c6cb580d2211c6370e
Wojciech Mazur MacBook <wmazur2@outlook.com>
2017-04-01 10:58:28 +0200

    Test

--- MachineSimulator/Program.cs
+++ MachineSimulator/Program.cs
@@ -13,7 +13,7 @@ namespace MachineSimulator
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {///eee
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
